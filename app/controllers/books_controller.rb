4637845de1dc54f2dc7b963d91b855288fd96efd
iwgsmnd <iwagoshi.mondo.infratop@gmail.com>
2018-12-05 10:02:36 +0000

    up

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,31 +2,35 @@ class BooksController < ApplicationController
   # def new
   #   @book = Book.new
   # end
-
-  def create
-    book = Book.new(book_params)
-    if book.save
-      flash[:success]='successfully'
-      redirect_to book_path(book.id)
-    else
-      flash[:denger]= 'erorr'
-    end
-  end
-
   def index
     @books = Book.all
     @book = Book.new
 
   end
 
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:success]='successfully'
+      redirect_to book_path(@book.id)
+    else
+      render :index
+    end
+  end
+
+
   def edit
     @book = Book.find(params[:id])
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:success]='successfully'
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
   end
 
   def show
